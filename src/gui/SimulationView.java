6903dd1228aade14b26fab7406922862b21437ce
lamprianidis <slamprianidis@trigonongroup.com>
2025-05-27 13:13:54 +0300

    Set simulation max duration to 60sec

--- src/gui/SimulationView.java
+++ src/gui/SimulationView.java
@@ -44,7 +44,7 @@ public class SimulationView extends Application {
     private Canvas canvas;
     private long simulationStartTime;
     private AnimationTimer timer;
-    private static final long maxDuration = 30_000; // 30 sec
+    private static final long maxDuration = 60_000; // 30 sec
 
     public static void setEnvironment(GridEnvironment environment) {
         env = environment;
